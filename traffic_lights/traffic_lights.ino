090e1a49407c641b7baa9eb594b94c38bc059860
Felikss Kamisevs <felix.fisherman@gmail.com>
2018-12-11 20:33:22 +0200

    Fixing bugs

--- traffic_lights/traffic_lights.ino
+++ traffic_lights/traffic_lights.ino
@@ -1,33 +1,32 @@
-﻿/**
-   Traffic light project
-*/
+/**
+ * Traffic lights project for Arduino
+ */
+const int carRed = 12; //car red light being assigned
+const int carYellow = 11;
+const int carGreen = 10;
 
+const int pedRed = 9; //pedestrian red light being assigned
+const int pedGreen = 8;
 
-int carRed = 12; //car red light being assigned
-int carYellow = 11;
-int carGreen = 10;
+const int button = 13; // button pin assigned
 
-int pedRed = 9; //pedestrian red light being assigned
-int pedGreen = 8;
-
-int button = 13; // button pin assigned
-
-int crossTime = 5000;
+const int crossTime = 5000;
 unsigned long carGreenTime; // collects the time since the car green light is on
 int minCarGreenTime = 5000; //minimum time cars will have green light ON
 
-int a = 0, b = 1, c = 2, d = 3, e = 4, f = 5, g = 6;
+const int a = 0, b = 1, c = 2, d = 3, e = 4, f = 5, g = 6;  // defining 7-sgement pin variables
 
 // ultrasonic sensor
-int trigPin = A0;
-int echoPin = A1;
+const int trigPin = A0;
+const int echoPin = A1;
 
 long duration;
 int distance;
 
-int speakerPin = A2;  // speaker pin
+const int speakerPin = A2;  // speaker pin
 
 void setup () {
+  // traffic lights setup
   pinMode(carRed, OUTPUT); // declare LED pins to be an output
   pinMode(carYellow, OUTPUT);
   pinMode(carGreen, OUTPUT);
@@ -42,36 +41,29 @@ void setup () {
   pinMode(echoPin, INPUT); // Sets the echoPin as an Input
 
   // 7 segment manual setup
-  pinMode(a, OUTPUT); // a
-  pinMode(b, OUTPUT); // b
-  pinMode(c, OUTPUT); // c
-  pinMode(d, OUTPUT); // d
-  pinMode(e, OUTPUT);   // e
-  pinMode(f, OUTPUT); // f
-  pinMode(g, OUTPUT);   // g
-
-  clearDisplay();
-  //tone(speakerPin, 262);
-
-  Serial.begin(9600);
+  pinMode(a, OUTPUT); 
+  pinMode(b, OUTPUT); 
+  pinMode(c, OUTPUT); 
+  pinMode(d, OUTPUT); 
+  pinMode(e, OUTPUT);  
+  pinMode(f, OUTPUT); 
+  pinMode(g, OUTPUT);  
+  clearDisplay(); // set all segments to LOW
 }
 
 void loop() {
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
-  // Sets the trigPin on HIGH state for 10 micro seconds
-  digitalWrite(trigPin, HIGH);
+  
+  digitalWrite(trigPin, HIGH); // Sets the trigPin on HIGH state for 10 micro seconds
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
-  // Reads the echoPin, returns the sound wave travel time in microseconds
-  duration = pulseIn(echoPin, HIGH);
-  // Calculating the distance
-  distance = duration * 0.034 / 2;
-  // Prints the distance on the Serial Monitor
-  Serial.print("Distance: ");
-  Serial.println(distance);
-
-  /* check if button is pressed
+  
+  duration = pulseIn(echoPin, HIGH); // Reads the echoPin, returns the sound wave travel time in microseconds
+  
+  distance = duration * 0.034 / 2; // Calculates the distance
+  
+  /* check if button is pressed or distance is small enough (<20 units)
     and it is over 5 sec since car green light is ON*/
   int state = digitalRead(button);
   if ((state == HIGH || distance < 20) && (millis() - carGreenTime) > minCarGreenTime) {
@@ -79,6 +71,9 @@ void loop() {
   }
 }
 
+/**
+ * Is triggered every time a button is pressed or distance to the sensor is less than 20 units
+ */
 void changeLights() {
   clearDisplay();
   delay(2000);
@@ -92,28 +87,23 @@ void changeLights() {
   digitalWrite(pedRed, LOW);
   digitalWrite(pedGreen, HIGH);
 
-  tone(speakerPin, 262);
+  tone(speakerPin, 262);  // sound signal
   delay(crossTime); //delay preset time of 5 seconds
-
+  
   noTone(speakerPin);
+  
+  // flashing of pedestrian green light for 5 seconds in total
   for (int i = 5; i >= 0; i--) {
     setSevenSegNumber(i);
     tone(speakerPin, 262);
-    delay(1000);
+    digitalWrite(pedGreen, HIGH);
+    delay(500);
     clearDisplay();
     noTone(speakerPin);
-  }
-
-  // flashing of pedestrian green light for 5 seconds in total, i.e. 10 iterations of 0.5 seconds
-  for (int x = 0; x < 10; x++) {
-    sevseg.refreshDisplay();
-    digitalWrite(pedGreen, HIGH);
-    delay(250);
     digitalWrite(pedGreen, LOW);
-    delay(250);
+    delay(500);
   }
-
-
+  
 
   digitalWrite(pedRed, HIGH);
   delay(100);
@@ -126,6 +116,10 @@ void changeLights() {
   carGreenTime = millis(); //record time since car green lights are on
 }
 
+/**
+ * Manually switches on pins of a single 7-segment display
+ * between 0 and 5
+ */
 void setSevenSegNumber(int n) {
   switch (n) {
     case 0:
@@ -173,6 +167,9 @@ void setSevenSegNumber(int n) {
   }
 }
 
+/**
+ * Resets the 7-segment display
+ */
 void clearDisplay() {
   digitalWrite(a, LOW);
   digitalWrite(b, LOW);
